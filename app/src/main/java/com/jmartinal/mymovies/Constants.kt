a51a8cfa069ecb57213d365c0310f3bc03b01f58
Jorge Martín Alvarado <jorgemartinalvarado@gmail.com>
2019-11-22 19:57:21 +0100

    New resources added

--- app/src/main/java/com/jmartinal/mymovies/Constants.kt
+++ app/src/main/java/com/jmartinal/mymovies/Constants.kt
@@ -6,6 +6,13 @@ class Constants {
             const val API_KEY = "a3280f1ae3f9bdab003ab1f6a35197d4"
             const val BASE_URL = "https://api.themoviedb.org/3/"
             const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w185/"
+            const val BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/"
+        }
+    }
+
+    class Communication {
+        companion object {
+            const val KEY_MOVIE = "movie"
         }
     }
 }
