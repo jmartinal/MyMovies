bcafebbeecaf3f109baf4b6a39e193bf6978bc4c
Jorge Martín Alvarado <jorgemartinalvarado@gmail.com>
2019-11-22 19:59:57 +0100

    Movie detail view implemented

--- app/src/main/java/com/jmartinal/mymovies/ui/MovieDetailActivity.kt
+++ app/src/main/java/com/jmartinal/mymovies/ui/MovieDetailActivity.kt
@@ -1,13 +1,50 @@
 package com.jmartinal.mymovies.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
+import com.jmartinal.mymovies.Constants
 import com.jmartinal.mymovies.R
+import com.jmartinal.mymovies.loadUrl
+import com.jmartinal.mymovies.model.Movie
+import kotlinx.android.synthetic.main.activity_movie_detail.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MovieDetailActivity : AppCompatActivity() {
+
+    private lateinit var movie: Movie
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
+        intent.extras?.apply {
+            movie = get(Constants.Communication.KEY_MOVIE) as Movie
+        }
+        movie.backdropPath?.let {
+            movieBackdrop.loadUrl(Constants.TmdbApi.BACKDROP_BASE_URL + it)
+        }
+        moviePoster.loadUrl(Constants.TmdbApi.POSTER_BASE_URL + movie.posterPath)
+        movieTitle.text = movie.title
+        movieReleaseDate.text =
+            LocalDate.parse(movie.releaseDate).format(DateTimeFormatter.ofPattern("MMMM dd, yyyy"))
+        movieRating.apply {
+            stepSize = 0.1f
+            rating = movie.voteAverage / 2
+        }
+        movieOverview.text = movie.overview
+        movieOriginalTitle.text = buildSpannedString {
+            bold { append(getString(R.string.original_title)) }
+            append(movie.originalTitle)
+        }
+        movieOriginalLanguage.text = buildSpannedString {
+            bold { append(getString(R.string.original_language)) }
+            append(movie.originalLanguage)
+        }
+        moviePopularity.text = buildSpannedString {
+            bold { append(getString(R.string.popularity)) }
+            append(movie.popularity.toString())
+        }
     }
 }
