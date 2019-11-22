69eafbaaec192a25d49ab92a48310acf0278da5d
Jorge Martín Alvarado <jorgemartinalvarado@gmail.com>
2019-11-22 20:00:27 +0100

    Navigation to Movie Detail view implemented

--- app/src/main/java/com/jmartinal/mymovies/ui/MainActivity.kt
+++ app/src/main/java/com/jmartinal/mymovies/ui/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.jmartinal.mymovies.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.jmartinal.mymovies.Constants
 import com.jmartinal.mymovies.R
 import com.jmartinal.mymovies.model.TmdbFactory.tmdbService
-import com.jmartinal.mymovies.toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 class MainActivity : AppCompatActivity() {
 
     private val adapter: MoviesAdapter = MoviesAdapter {
-        toast(it.title)
+        Intent(this@MainActivity, MovieDetailActivity::class.java).apply {
+            putExtra(Constants.Communication.KEY_MOVIE, it)
+            startActivity(this)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
