c24219150f84dfef6b497aa7c4c7e1cf065d8bc2
Jorge Martín Alvarado <jorgemartinalvarado@gmail.com>
2019-12-08 18:53:14 +0100

    Remove identical if branches

--- app/src/main/java/com/jmartinal/mymovies/model/TmdbFactory.kt
+++ app/src/main/java/com/jmartinal/mymovies/model/TmdbFactory.kt
@@ -1,6 +1,5 @@
 package com.jmartinal.mymovies.model
 
-import android.os.Build
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.jmartinal.mymovies.BuildConfig
 import com.jmartinal.mymovies.Constants
@@ -13,17 +12,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 object TmdbFactory {
 
     private val tmdbInterceptor = Interceptor { chain ->
-        val newUrl = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        val newUrl =
             chain.request().url()
                 .newBuilder()
                 .addQueryParameter("api_key", Constants.TmdbApi.API_KEY)
                 .build()
-        } else {
-            chain.request().url()
-                .newBuilder()
-                .addQueryParameter("api_key", Constants.TmdbApi.API_KEY)
-                .build()
-        }
+
 
         val newRequest = chain.request()
             .newBuilder()
@@ -36,7 +30,8 @@ object TmdbFactory {
     val client = OkHttpClient().newBuilder()
         .addInterceptor(tmdbInterceptor)
         .addInterceptor(HttpLoggingInterceptor().apply {
-            level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+            level =
+                if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
         })
         .build()
 
