7b7b63732f547398a0df5c0907b9d9572c4b4f43
Jorge Martín Alvarado <jorgemartinalvarado@gmail.com>
2020-02-20 21:14:33 +0100

    Mocks class created
    (cherry picked from commit 6c794c14e85fbe09c947ac3f071106711604f4a5)

---
+++ testShared/src/main/java/com/jmartinal/testshared/Mocks.kt
@@ -0,0 +1,21 @@
+package com.jmartinal.testshared
+
+import com.jmartinal.domain.Movie
+
+val mockedMovie = Movie(
+    1,
+    false,
+    "",
+    false,
+    "EN",
+    "Original title",
+    "Overview",
+    1.0F,
+    "",
+    "01/01/2020",
+    "Title",
+    false,
+    7.5F,
+    1234
+
+)
